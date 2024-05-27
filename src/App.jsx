c6bafc9f60f59f4268add869846d9dedd79a368e
PenguinPush <icyzminecraft@gmail.com>
2024-05-26 23:22:08 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, {useEffect} from 'react';
+import './App.css';
+import {Player} from "textalive-app-api";
+
+function App() {
+    const animateWord = function (now, unit) {
+        if (unit.contains(now)) {
+            document.querySelector("#text").textContent = unit.text;
+        }
+    };
+
+    const player = new Player({
+        app: {
+            token: "voiEWpeaIFwNII7p",
+        }
+    });
+
+    player.addListener({
+        onVideoReady: (v) => {
+            let w = player.video.firstWord;
+            while (w) {
+                w.animate = animateWord;
+                w = w.next;
+            }
+        },
+    });
+
+    player.addListener({
+        onAppReady: (app) => {
+            if (!app.songUrl) {
+                player.createFromSongUrl("https://piapro.jp/t/Rejk/20240202164429", {
+                    video: {
+                        beatId: 4592298,
+                        chordId: 2727638,
+                        repetitiveSegmentId: 2824329,
+                        lyricId: 59418,
+                        lyricDiffId: 13965
+                    },
+                });
+            }
+            if (!app.managed) {
+                // showControls();
+            }
+        },
+    });
+
+    player.addListener({
+        onTimeUpdate: (position) =>
+            console.log(position),
+    });
+
+    return (
+        <>
+            <h1>sekaiiii</h1>
+            <span id="text"></span>
+        </>
+    );
+}
+
+export default App;
