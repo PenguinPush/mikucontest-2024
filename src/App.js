3455e2752d26a0c4dde4901285c08a0f4c673244
_Fushigidane_ <84942946+Fushigidane-Snom@users.noreply.github.com>
2024-06-23 16:03:24 -0400

    Add constants file

--- src/App.js
+++ src/App.js
@@ -27,20 +27,7 @@ const positionDisplay = document.querySelector("#position strong");
 const artistSpan = document.querySelector("#artist-name span");
 const songSpan = document.querySelector("#song-name span");
 
-const songList = [
-    ["https://piapro.jp/t/hZ35/20240130103028", 4592293, 2727635, 2824326, 59415, 13962],
-    ["https://piapro.jp/t/--OD/20240202150903", 4592296, 2727636, 2824327, 59416, 13963],
-    ["https://piapro.jp/t/XiaI/20240201203346", 4592297, 2727637, 2824328, 59417, 13964],
-    ["https://piapro.jp/t/Rejk/20240202164429", 4592298, 2727638, 2824329, 59418, 13965],
-    ["https://piapro.jp/t/ELIC/20240130010349", 4592299, 2727639, 2824330, 59419, 13966],
-    ["https://piapro.jp/t/xEA7/20240202002556", 4592300, 2727640, 2824331, 59420, 13967]
-];
-
-// constants
-const maxTextScale = 1.1;
-const minTextScale = 0.9;
-const baseTextSize = 8;
-const fov = 60;
+import { maxTextScale, minTextScale, baseTextSize, fov, songList } from "./constants";
 
 // textalive
 let player;
---
+++ src/constants.js
@@ -0,0 +1,15 @@
+// constants
+export const maxTextScale = 1.1;
+export const minTextScale = 0.9;
+export const baseTextSize = 8;
+export const fov = 60;
+
+export const songList = [
+    ["https://piapro.jp/t/hZ35/20240130103028", 4592293, 2727635, 2824326, 59415, 13962],
+    ["https://piapro.jp/t/--OD/20240202150903", 4592296, 2727636, 2824327, 59416, 13963],
+    ["https://piapro.jp/t/XiaI/20240201203346", 4592297, 2727637, 2824328, 59417, 13964],
+    ["https://piapro.jp/t/Rejk/20240202164429", 4592298, 2727638, 2824329, 59418, 13965],
+    ["https://piapro.jp/t/ELIC/20240130010349", 4592299, 2727639, 2824330, 59419, 13966],
+    ["https://piapro.jp/t/xEA7/20240202002556", 4592300, 2727640, 2824331, 59420, 13967]
+];
+
